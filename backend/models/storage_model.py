9333c0a588e8f3bd642d0f30ebaa66d1196aba45
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2025-09-20 10:36:19 -0700

    Delete backend directory

--- backend/models/storage_model.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class FileUploadResponse(BaseModel):
-    status: str
-    public_url: str
--- backend/models/user_sheets_model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Dict, List, Optional
-
-class UserSheetRequest(BaseModel):
-    user_id: int
-    sheet_name: str
-    columns: Dict[str, str]  # Example: {"name": "string", "price": "float"}
-    employee_access: Optional[List[int]] = []
